296a0ee5b96f58930353302a53c9d038561f6894
Domryuken <ladonjackson@hotmail.co.uk>
2019-11-12 22:07:23 +0000

    update nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Home} from './pages/Home';
 import {OurCoffee} from './pages/OurCoffee';
 import {ContactUs} from './pages/ContactUs';
 import {Events} from './pages/Events';
 import {FindUs} from './pages/FindUs';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {Nav, Navbar} from 'react-bootstrap'
+import Image from 'react-bootstrap/Image'
+
 
 class App extends Component {
   render() {
@@ -15,19 +18,18 @@ class App extends Component {
         <React.Fragment>
           <Navbar bg="light" expand="lg">
             <Navbar.Brand href="/">
-              <img
-                alt=""
-                src="/coffeehouselogo.png"
-                width="30"
-                height="30"
+              <Image src="/coffeehouselogo.png"
+                width="50"
+                height="50"
                 className="d-inline-block align-top"
+                roundedCircle fluid
               />
               {' Some Coffee Shop'}
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto">
-                <Nav.Link href="/">OUR COFFEE</Nav.Link>
+                <Nav.Link href="/our-coffee">OUR COFFEE</Nav.Link>
                 <Nav.Link href="/events">EVENTS</Nav.Link>
                 <Nav.Link href="/find-us">FIND US</Nav.Link>
                 <Nav.Link href="/contact-us">CONTACT US</Nav.Link>
@@ -39,7 +41,8 @@ class App extends Component {
         <React.Fragment>
           <BrowserRouter>
             <Switch>
-              <Route exact path="/" component={OurCoffee}/>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/our-coffee" component={OurCoffee}/>
               <Route exact path="/events" component={Events}/>
               <Route exact path="/find-us" component={FindUs}/>
               <Route exact path="/contact-us" component={ContactUs}/>
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'react-bootstrap/Image'
+
+export const Home = () => (
+    <div>
+        <div>
+            <Image src="/home.jpg" fluid />
+        </div>
+        <div>
+            Home
+        </div>
+    </div>
+)
